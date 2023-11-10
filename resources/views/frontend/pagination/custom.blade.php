a212c20a36bc82595a4c774eebd3cb86b61bb600
Kishan Rathod <kishan@auctionsoftware.com>
2023-11-10 19:19:40 +0530

    product listing

--- resources/views/frontend/pagination/custom.blade.php
+++ resources/views/frontend/pagination/custom.blade.php
@@ -2,7 +2,9 @@
 <ul class="pagination__wrapper d-flex align-items-center justify-content-center">
     @if ($paginator->onFirstPage())
     <li class="pagination__list disabled">
-        <span class="pagination__item pagination__item--current">← Previous</span>
+        <span class="pagination__item pagination__item--current">
+            <svg xmlns="http://www.w3.org/2000/svg"  width="22.51" height="20.443" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M244 400L100 256l144-144M120 256h292"/></svg>
+        </span>
     </li>
     @else
     <li class="pagination__list" >
