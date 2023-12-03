af57d497b49e3af95bfb1e759e8ee08a6b8207c8
Kishan Rathod <kishan@auctionsoftware.com>
2023-12-03 12:46:53 +0530

    admin css

--- resources/views/backend/layouts/layout.blade.php
+++ resources/views/backend/layouts/layout.blade.php
@@ -21,7 +21,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700">
 
 	<!-- aiz core css -->
-	<link rel="stylesheet" href="{{ static_asset('assets/css/vendors.css') }}">
+	<link rel="stylesheet" href="{{ static_asset('assets/css/vendors_admin.css') }}">
     @if(\App\Models\Language::where('code', Session::get('locale', Config::get('app.locale')))->first()->rtl == 1)
     <link rel="stylesheet" href="{{ static_asset('assets/css/bootstrap-rtl.min.css') }}">
     @endif
